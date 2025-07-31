c47af66a901c4f3f0d7043c0db970c8ae9f5e2a5
virtual-greenjay <joellbrink02@gmail.com>
2025-07-31 15:08:18 +0200

    GraphSearch WIP

--- Assets/Scripts/Tools/Graph.cs
+++ Assets/Scripts/Tools/Graph.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Graph<T>
@@ -12,7 +13,10 @@ public class Graph<T>
         if (!adjacencyList.ContainsKey(node))
         {
             adjacencyList[node] = new List<T>();
-        } 
+        } else
+        {
+            Debug.Log("Node already exists.");
+        }
     }
     public void AddEdge(T fromNode, T toNode) {
         if (!adjacencyList.ContainsKey(fromNode) || !adjacencyList.ContainsKey(toNode))
@@ -40,10 +44,78 @@ public class Graph<T>
             foreach (T adj in node.Value) {
                 adjacents += adj.ToString() + ", ";
             }
-            adjacents = adjacents.Substring(0, adjacents.Length - 2);
+            adjacents = adjacents.Substring(0, adjacents.Length - 2); //removes the last comma and space
 
             Debug.Log(String.Format("The node {0} is next to {1}", node.Key, adjacents));
         }
     }
 
+    public void BFS(T startingNode)
+    {
+        //Uses Queue, FIFO method
+        Queue<T> Q = new Queue<T>();
+        List<T> discovered = new List<T>();
+
+        Q.Enqueue(startingNode);
+        discovered.Add(startingNode);
+
+        while (Q.Count > 0) {
+            var currentNode = Q.Dequeue();
+
+            // do something with the node
+
+            //add adjacent nodes to Queue
+            foreach (T childNode in adjacencyList[currentNode])
+            {
+                if (!discovered.Contains(childNode))
+                {
+                    Q.Enqueue(childNode);
+                    discovered.Add(childNode);
+                }
+            }
+
+
+
+            //first v. less elegant. should be equal but keep in case v2 doesn't work
+            //for (int i = 0; i < adjacencyList[currentNode].Count; i++)
+            //{
+            //    if (!discovered.Contains(adjacencyList[currentNode][i]))
+            //    {
+            //        Q.Enqueue(adjacencyList[currentNode][i]);
+            //        discovered.Add(adjacencyList[currentNode][i]);
+            //    }
+            //}
+
+        }
+        //compare list of discovered and keys in Adjacency list. 
+
+    }
+
+    public void DFS(T startingNode)
+    {
+        //Uses Stack, FILO method
+        Stack<T> S = new Stack<T>();
+        List<T> discovered = new List<T>();
+
+        S.Push(startingNode);
+        discovered.Add(startingNode);
+
+        while (S.Count > 0)
+        {
+            var currentNode = S.Pop();
+
+            // do something with the node
+
+            //add adjacent nodes to Stack
+            foreach (T childNode in adjacencyList[currentNode])
+            {
+                if (!discovered.Contains(childNode))
+                {
+                    S.Push(childNode);
+                    discovered.Add(childNode);
+                }
+            }
+        }
+        //compare list of discovered and keys in Adjacency list. 
+    }
 }
